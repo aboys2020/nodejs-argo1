b49aa33dad23aa862e9b6b87c285404688171131
Erick Siglow <82213215+aboys2020@users.noreply.github.com>
2026-03-04 00:05:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'newtest.asonga.ccwu.cc';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMGE0MDViODllMTZjODY0ZWM5ZTIwZWY1ODI5ZWEwYTEiLCJ0IjoiZDkxZDEzMzUtZDNjNC00YjRkLWJlZDQtNzJmZWQyMTIzZDFkIiwicyI6Ik5UTmhPV0l6T1dRdFpUZzJZaTAwWldSa0xUbG1aalV0TVRVME1qRm1NbVkxWmpWaSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'yg7.ygkkk.dpdns.org';            // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'cdns.doon.eu.org';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'newtest';                        // 节点名称
 
